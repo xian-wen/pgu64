238445a357997657906f01e810b83bfd4ee1074e
xian-wen <xianwen_yin@163.com>
2022-06-03 08:20:35 +0800

    Add exit.s

---
+++ examples/exit.s
@@ -0,0 +1,17 @@
+# PURPOSE: simple program exit and return a status code. 
+#
+# VARIABLES:
+#   %rax - holds the system call number
+#   %rbx - holds the return value
+
+.section .data
+
+.section .text
+.globl _start
+
+_start:
+    movq $60, %rax  # exit
+
+    movq $0, %rbx
+
+    syscall
